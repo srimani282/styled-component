36b461fa81b568636b44a2d466b721ab8c2116ce
sri <srimanigoud@gmail.com>
2020-10-26 14:34:58 +0530

    updated to my

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -3,7 +3,7 @@ import StripeCheckout from 'react-stripe-checkout';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_WBqax2FWVzS9QlpJScO07iuL';
+  const publishableKey = 'pk_test_51Hef6gCe0NKgeWwCxBJ8kILg8ljfmkqrZSgEtf0fJ7KUciKSxjF6MuG2CjSBMOwGnKCNQhsSdjB0YdBw2LcvQbDk00hqcXJvYS';
 
   const onToken = token => {
     console.log(token);
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBd5z45uGZdGuIChbHLxtUg6GxB7Dlv-mw",
+    authDomain: "react-kummu.firebaseapp.com",
+    databaseURL: "https://react-kummu.firebaseio.com",
+    projectId: "react-kummu",
+    storageBucket: "react-kummu.appspot.com",
+    messagingSenderId: "709816197032",
+    appId: "1:709816197032:web:0d9f56bfa0b8211967c79d",
+    measurementId: "G-9PNJ5PV9ZY"
 };
 
 firebase.initializeApp(config);
