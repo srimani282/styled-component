6841b4ef8da49d82676c0851654b04dd530b44a1
sri <srimanigoud@gmail.com>
2020-10-28 17:07:07 +0530

    updated to hoc

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 const config = {
   apiKey: "AIzaSyBd5z45uGZdGuIChbHLxtUg6GxB7Dlv-mw",
-    authDomain: "react-kummu.firebaseapp.com",
-    databaseURL: "https://react-kummu.firebaseio.com",
-    projectId: "react-kummu",
-    storageBucket: "react-kummu.appspot.com",
-    messagingSenderId: "709816197032",
-    appId: "1:709816197032:web:0d9f56bfa0b8211967c79d",
-    measurementId: "G-9PNJ5PV9ZY"
+  authDomain: "react-kummu.firebaseapp.com",
+  databaseURL: "https://react-kummu.firebaseio.com",
+  projectId: "react-kummu",
+  storageBucket: "react-kummu.appspot.com",
+  messagingSenderId: "709816197032",
+  appId: "1:709816197032:web:0d9f56bfa0b8211967c79d",
+  measurementId: "G-9PNJ5PV9ZY"
 };
 
 firebase.initializeApp(config);
@@ -39,6 +39,41 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 
   return userRef;
 };
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach(obj => {
+
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+
+  });
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+}
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
